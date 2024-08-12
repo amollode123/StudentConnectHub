a798dff6105364a9116e987e7427dd33b908c1b4
AMOL <amollode123@gmail.com.com>
2024-08-13 00:17:51 +0530

    StudentConnectHub

---
+++ src/main/java/com/StudentConnectHub/StudentConnectHubApplication.java
@@ -0,0 +1,13 @@
+package com.StudentConnectHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentConnectHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentConnectHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StudentConnectHub/StudentConnectHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentConnectHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentConnectHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
